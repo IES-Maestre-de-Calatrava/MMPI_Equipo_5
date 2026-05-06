bb081ca813280ea91aba7820e204cd91c02dc12b
Agustín Santiago <agustinesantiagos@gmail.com>
2026-05-06 10:51:50 +0200

    f
    f

--- MMPI_API_Eclipse/mmpi/src/main/java/com/example/mmpi/repository/AccesoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.mmpi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.mmpi.persistence.Acceso;
-
-public interface AccesoRepository extends JpaRepository<Acceso, Long> {
-
-}
--- MMPI_API_Eclipse/mmpi/src/main/java/com/example/mmpi/repository/EventoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.mmpi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.mmpi.persistence.Evento;
-
-public interface EventoRepository extends JpaRepository<Evento, Long> {
-
-}
