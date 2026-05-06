6a64eeebd36de2958eb05959a246dee7bb5c2606
Agustín Santiago <agustinesantiagos@gmail.com>
2026-05-06 13:19:07 +0200

    Eliminacion de Rama Erronea
    Putos fuegos

--- Rama de Jose 🔥🔥🔥/mmpi/src/main/java/com/example/mmpi/repository/FisioterapeutaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.mmpi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.mmpi.persistence.Fisioterapeuta;
-
-public interface FisioterapeutaRepository extends JpaRepository<Fisioterapeuta, String> {
-
-}
